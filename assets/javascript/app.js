7bbf5a008af9c5ae2229546acdaa9d0586dad029
Jennifer Rose <jenniferrose@Jennifers-MacBook-Pro-2.local>
2016-05-21 10:45:02 -0400

    map with pins

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,6 @@
 // Javascript function that wraps everything
 $(document).ready(function(){
 
-	//map
-	function initMap() {
-        var mapDiv = document.getElementById('map');
-        var map = new google.maps.Map(mapDiv, {
-          center: {lat: 44.540, lng: -78.546},
-          zoom: 8
-        });
-      }
 
 	function plydt() {
 
@@ -32,54 +24,49 @@ $(document).ready(function(){
 		// functions
 
 		//map functions
-			function initMap() {
-			    var mapDiv = document.getElementById('map');
-			    var map = new google.maps.Map(mapDiv, {
-			      center: {lat: 44.540, lng: -78.546},
-			      zoom: 8
-			    });
 
-			   	// var service = new google.maps.PlacesService(map);
-			   	// var request = {
-			   	// 	location: new.google.maps.LatLng(44.540, -78.546),	
-			   	// 	radius: 45000,		  
-			   	// 	keyword: 'kids play area near me'
-			   	// };
-			  }
+			// var map;
+			// var service;
+			// var infowindow;
 
-			$('#map').append(map);
+			// function initialize() {
+			//   var pyrmont = new google.maps.LatLng(-44.540,-78.546);
 
-			var map;
-			var service;
-			var infowindow;
+			//   map = new google.maps.Map(document.getElementById('map'), {
+			//       center: pyrmont,
+			//       zoom: 8
+			//     });
 
-			function initialize() {
-			  var pyrmont = new google.maps.LatLng(-44.540,-78.546);
+			//   var request = {
+			//     location: pyrmont,
+			//     radius: '50000',
+			//     keyword: 'kids play area near me'
+			//   };
 
-			  map = new google.maps.Map(document.getElementById('map'), {
-			      center: pyrmont,
-			      zoom: 8
-			    });
+			//   service = new google.maps.places.PlacesService(map);
+			//   service.textSearch(request, callback);
+			// }
 
-			  var request = {
-			    location: pyrmont,
-			    radius: '500',
-			    keyword: 'kids play area near me'
-			  };
+			// function callback(results, status) {
+			//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+			//     for (var i = 0; i < results.length; i++) {
+			//       var place = results[i];
+			//       createMarker(results[i]);
+			//     }
+			//   }
+			// }
+			// function createMarker(place) {
+			// 	var placeLoc = place.geometry.location;
+			// 	var marker = new google.maps.Marker({
+			// 	map: map,
+			// 	position: place.geometry.location
+			// 	});
 
-			  service = new google.maps.places.PlacesService(map);
-			  service.textSearch(request, callback);
-			}
-
-			function callback(results, status) {
-			  if (status == google.maps.places.PlacesServiceStatus.OK) {
-			    for (var i = 0; i < results.length; i++) {
-			      var place = results[i];
-			      createMarker(results[i]);
-			    }
-			  }
-			}
-
+			// 	google.maps.event.addListener(marker, 'click', function() {
+			// 	infowindow.setContent(place.name);
+			// 	infowindow.open(map, this);
+			// 	});
+			// 	}
 			
 		//end map functions
 
