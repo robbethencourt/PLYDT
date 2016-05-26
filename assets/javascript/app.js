70a76690ab3ab1dace22a963b92c004547e17890
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-05-26 14:20:41 -0400

    removing the node modules folders

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -180,6 +180,22 @@ $(document).ready(function(){
 
 		});
 
+		//Start google auth
+
+
+		$('#loginbutton').on('click', function(){
+			var ref = new Firebase('https://plydt.firebaseio.com/');
+			ref.authWithOAuthRedirect("google", function(error, authData){
+				if(error){
+					console.log("Login Failed!", error);
+				} else{
+					console.log("Authenticated successfully with payload:", authData);
+				}
+				console.log(authData);
+			})
+					
+		});
+
 		// create user button click event if they are searching
 		$('#create-user').on('click', function() {
 
@@ -648,7 +664,7 @@ function initMap() {
 	    styles: [{
 	      	where: 'Icon',
 	  		markerOptions: {
-	    		iconName: "purple_stars"
+	    		iconName: "sunny"
 	  		},       		
 	    }]
      }); //end preferred list layer
